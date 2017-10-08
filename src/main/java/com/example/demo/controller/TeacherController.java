ae92ab795d9f519e8c22254db85980d85a7ef8c8
Alexander Londoño Espejo <alexlondon07@secuencia24.com>
2017-10-07 20:09:50 -0500

    TeacherController

--- src/main/java/com/example/demo/controller/TeacherController.java
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -1,18 +1,32 @@
 package com.example.demo.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.model.Teacher;
 import com.example.demo.service.TeacherService;
+import com.example.demo.util.CustomErrorType;
 
 @Controller
 @RequestMapping(value="/v1")
@@ -23,26 +37,110 @@ public class TeacherController {
 	
 	// ------------------- GET Teacher-----------------------------------------
 	@RequestMapping(value = "/teachers", method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<List<Teacher>> getTeachers(@RequestParam(value="name", required = false) String name, @RequestParam(value = "id_teacher", required = false) Long id_teacher){
-		
+    public ResponseEntity<List<Teacher>> getTeachers(@RequestParam(value="name", required=false) String name){
 		List<Teacher> teachers = new ArrayList<Teacher>();
 		
-		if(name != null){
+		if (name == null) {
 			teachers = _teacherService.findAllTeachers();
-			if(teachers.isEmpty()){
-				return new ResponseEntity(HttpStatus.NO_CONTENT);
-				// You many decide to return HttpStatus.NOT_FOUND
+	        if (teachers.isEmpty()) {
+	            return new ResponseEntity(HttpStatus.NO_CONTENT);
+	            // You many decide to return HttpStatus.NOT_FOUND
+	        }
+		   
+			return new ResponseEntity<List<Teacher>>(teachers, HttpStatus.OK);
+		} else {
+			Teacher teacher = _teacherService.findByName(name);
+			if (teacher == null) {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+			
+			teachers.add(teacher);
+			return new ResponseEntity<List<Teacher>>(teachers, HttpStatus.OK);
+		}
+    }
+	
+	// ------------------- GET Teacher Find By Id-----------------------------------------
+	@RequestMapping(value = "/teachers/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<Teacher> getCourseById(@PathVariable("id") Long id){
+		Teacher teacher = _teacherService.findById(id);
+        if (teacher == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            // You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<Teacher>(teacher, HttpStatus.OK);
+    }
+	
+	
+	// ------------------- POST Teacher-----------------------------------------
+	public static final String TEACHER_UPLOADED_FOLDER ="images/teachers/";
+	//CREATE TEACHER IMAGE
+	@RequestMapping(value="/teachers/images", method = RequestMethod.POST, headers=("content-type=multipart/form-data"))
+	public ResponseEntity<byte[]> uploadTeacherImage(@RequestParam("id_teacher") Long idTeacher, 
+			@RequestParam("file") MultipartFile multipartFile, 
+			UriComponentsBuilder componentsBuilder){
+		if (idTeacher == null) {
+			return new ResponseEntity(new CustomErrorType("Please set id_teacher"), HttpStatus.NO_CONTENT);
+		}
+		
+		if (multipartFile.isEmpty()) {
+			return new ResponseEntity(new CustomErrorType("Please select a file to upload"), HttpStatus.NO_CONTENT);
+		}
+		
+		Teacher teacher = _teacherService.findById(idTeacher);
+		if (teacher == null) {
+			return new ResponseEntity(new CustomErrorType("Teacher with id_teacher: " + idTeacher + " not dfound"), HttpStatus.NOT_FOUND);
+		}
+		
+		if (!teacher.getAvatar().isEmpty() || teacher.getAvatar() != null) {
+			String fileName = teacher.getAvatar();
+			Path path = Paths.get(fileName);
+			File f = path.toFile();
+			if (f.exists()) {
+				f.delete();
 			}
 		}
 		
-		return null;
-		
+		try {
+			Date date = new Date();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+			String dateName = dateFormat.format(date);
+			
+			String fileName = String.valueOf(idTeacher) + "-pictureTaecher-" + dateName + "." + multipartFile.getContentType().split("/")[1];
+			teacher.setAvatar(TEACHER_UPLOADED_FOLDER + fileName);
+			
+			byte[] bytes = multipartFile.getBytes();
+			Path path = Paths.get(TEACHER_UPLOADED_FOLDER + fileName);
+			Files.write(path, bytes);
+			
+			_teacherService.updateTeacher(teacher);
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new ResponseEntity(new CustomErrorType("Error during upload: " + multipartFile.getOriginalFilename()),HttpStatus.CONFLICT);
+		}
 	}
-	
-	// ------------------- POST Teacher-----------------------------------------
 	
 	// ------------------- UPDATE Teacher-----------------------------------------
 	
+	
+	
+	
+	
+	
+	
 	// ------------------- DELETE Teacher-----------------------------------------
+	@RequestMapping(value = "/teachers/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public ResponseEntity<?> deleteCourse(@PathVariable("id") Long id) {
+		Teacher teacher = _teacherService.findById(id);
+        if (teacher == null) {
+        	
+            return new ResponseEntity(new CustomErrorType("Unable to delete. teacher with id " + id + " not found."),
+                    HttpStatus.NOT_FOUND);
+        }
+        
+        _teacherService.deleteTeacherById(id);
+        return new ResponseEntity<Teacher>(HttpStatus.NO_CONTENT);
+    }
 	
 }
